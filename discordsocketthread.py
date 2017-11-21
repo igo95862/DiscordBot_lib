9c45ccae99f1e12f8e4f0de17176a9f5fa6a534c
igo95862 <igo95862@yandex.ru>
2017-11-21 12:57:15 -0800

    Redesigned discord socket thread init. Added 'event_hook_add' function

--- discordsocketthread.py
+++ discordsocketthread.py
@@ -1,30 +1,27 @@
 import discordsocket
 import asyncio
 import threading
+from functools import partial as func_partial
 
 
 class DiscordSocketThread:
 
-    def __init__(self, discord_websocket: discordsocket.DiscordSocket):
-        self.discord_socket = discord_websocket
-        self.discord_socket_loop = asyncio.new_event_loop()
-        self.discord_socket.event_loop = self.discord_socket_loop
+    def _init(self, *args):
+
+        if type(args[0]) is str:
+            # NOTE: check that the string has proper length and etc.?
+            self.discord_socket_loop = asyncio.new_event_loop()
+            self.discord_socket = discordsocket.DiscordSocket(args[0], event_loop=self.discord_socket_loop)
+        elif type(args[0]) is discordsocket.DiscordSocket:
+            self.discord_socket = args[0]
+            self.discord_socket.event_loop = self.discord_socket_loop
+        else:
+            raise TypeError('Neither token nor socket was passed to socket.')
+
+        self.discord_socket_loop.create_task(self.discord_socket.init())
 
         self.thread = threading.Thread(target=self.discord_socket_loop.run_forever)
         self.thread.start()
-
-        connect_future = asyncio.run_coroutine_threadsafe(self.discord_socket._connect(),
-                                                          self.discord_socket_loop)
-        connect_future.result(timeout=5)
-
-        identify_future = asyncio.run_coroutine_threadsafe(self.discord_socket._identify(),
-                                                           self.discord_socket_loop)
-        identify_future.result(timeout=5)
-
-        self.heartbeat_future = asyncio.run_coroutine_threadsafe(self.discord_socket._heartbeat_cycle(),
-                                                                 self.discord_socket_loop)
-        self.receive_future = asyncio.run_coroutine_threadsafe(self.discord_socket._receive_cycle(),
-                                                               self.discord_socket_loop)
 
     def request_guild_members(self, guild_id: int, query: str = '', limit: int = 0):
         return asyncio.run_coroutine_threadsafe(self.discord_socket.request_guild_members(guild_id, query, limit),
@@ -39,4 +36,10 @@ class DiscordSocketThread:
     def voice_state_update(self, guild_id: int, channel_id: int = None,
                            mute_self: bool = None, deaf_self: bool = False):
         return asyncio.run_coroutine_threadsafe(self.discord_socket.voice_state_update(guild_id, channel_id,
-                                                                                       mute_self, deaf_self))
+                                                                                       mute_self, deaf_self),
+                                                self.discord_socket_loop)
+
+    def event_hook_add(self, coroutine):
+        self.discord_socket_loop.call_soon_threadsafe(func_partial(self.discord_socket.event_hooks.append, coroutine))
+
+
