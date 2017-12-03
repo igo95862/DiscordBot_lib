debb0940eea67ce2fa403fb1123695d0d43a5a68
igo95862 <igo95862@yandex.ru>
2017-12-02 21:23:13 -0800

    discordbot: -Preliminary rate limiter

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,32 @@
 import discordrest
 import discordsocketthread
+import time
+import functools
 
-class discordbot:
 
-    def __init__(self):
-        pass
+class DiscordBot:
 
-    def rate_limiter_test(self, func):
-        pass
+    def __init__(self, token: str):
+        self.discord_session = discordrest.DiscordSession(token=token)
+        self.rate_limit_table = {}
 
+    def rate_limiter_test(self, func_partial: functools.partial):
+        func = func_partial.func
+
+        try:
+            remaining_limit = self.rate_limit_table[func][0]
+        except KeyError:
+            self.rate_limit_table[func] = None
+            remaining_limit = -1
+
+        if remaining_limit == 0:
+            sleep_time = self.rate_limit_table[func][1] - time.time()
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+
+        response = func_partial()
+
+        self.rate_limit_table[func] = (int(response.headers['X-RateLimit-Remaining']),
+                                       int(response.headers['X-RateLimit-Reset']))
+
+        return response
