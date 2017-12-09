92daeea3621ae379ab95cdd832018d5771c74471
igo95862 <igo95862@yandex.ru>
2017-12-08 16:21:57 -0800

    discordsocketthread: -Changed formatting a bit to make it more readable.

--- discordsocketthread.py
+++ discordsocketthread.py
@@ -24,20 +24,24 @@ class DiscordSocketThread:
         self.thread.start()
 
     def request_guild_members(self, guild_id: int, query: str = '', limit: int = 0):
-        return asyncio.run_coroutine_threadsafe(self.discord_socket.request_guild_members(guild_id, query, limit),
-                                                self.discord_socket_loop)
+        return asyncio.run_coroutine_threadsafe(
+            self.discord_socket.request_guild_members(
+                guild_id, query, limit),
+            self.discord_socket_loop)
 
     def status_update(self, status_type: str, is_afk: bool,
                       game: dict = None, since_time_seconds: int = None):
-        return asyncio.run_coroutine_threadsafe(self.discord_socket.status_update(status_type, is_afk,
-                                                                                  game, since_time_seconds),
-                                                self.discord_socket_loop)
+        return asyncio.run_coroutine_threadsafe(
+            self.discord_socket.status_update(
+                status_type, is_afk, game, since_time_seconds),
+            self.discord_socket_loop)
 
     def voice_state_update(self, guild_id: int, channel_id: int = None,
                            mute_self: bool = None, deaf_self: bool = False):
-        return asyncio.run_coroutine_threadsafe(self.discord_socket.voice_state_update(guild_id, channel_id,
-                                                                                       mute_self, deaf_self),
-                                                self.discord_socket_loop)
+        return asyncio.run_coroutine_threadsafe(
+            self.discord_socket.voice_state_update(
+                guild_id, channel_id, mute_self, deaf_self),
+            self.discord_socket_loop)
 
     def event_hook_add(self, coroutine):
         self.discord_socket_loop.call_soon_threadsafe(func_partial(self.discord_socket.event_hooks.append, coroutine))
