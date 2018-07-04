65620c5730eb9086294b8be9d79b08bc4729fa8c
igo95862 <igo95862@yandex.ru>
2018-07-03 21:31:42 -0700

    Fixed guild_channel_modify of DiscordClientAsync.

--- discordobjects/client/client_async.py
+++ discordobjects/client/client_async.py
@@ -338,24 +338,12 @@ class DiscordClientAsync:
             new_nsfw: bool = None,
             new_bitrate: int = None, new_user_limit: int = None, new_overwrite_array: list = None,
             new_parent_id: str = None):
-        params = {}
-        if new_name is not None:
-            params['name'] = new_name
-        if new_position is not None:
-            params['position'] = new_position
-        if new_topic is not None:
-            params['topic'] = new_topic
-        if new_nsfw is not None:
-            params['nsfw'] = new_nsfw
-        if new_bitrate is not None:
-            params['bitrate'] = new_bitrate
-        if new_user_limit is not None:
-            params['userlimit'] = new_user_limit
-        if new_overwrite_array is not None:
-            params['permission_overwrites'] = new_overwrite_array
-        if new_parent_id is not None:
-            params['parent_id'] = new_parent_id
-        return await self.rate_limit(f_partial(self.rest_session.channel_modify, channel_id, params))
+        return await self.rate_limit(
+            f_partial(
+                self.rest_session.channel_modify, channel_id,
+                new_name, new_position, new_topic,
+                new_nsfw, new_bitrate, new_user_limit,
+                new_overwrite_array, new_parent_id))
 
     async def channel_delete(self, channel_id: str) -> dict:
         return await self.rate_limit(f_partial(self.rest_session.channel_delete, channel_id))
