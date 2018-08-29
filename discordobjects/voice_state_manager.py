cb68e8e4ee1c5397da396265d898b64dcfbfcd33
igo95862 <igo95862@yandex.ru>
2018-08-29 09:54:25 -0700

    Working on voice state manager

--- discordobjects/voice_state_manager.py
+++ discordobjects/voice_state_manager.py
@@ -8,7 +8,7 @@ class VoiceStateManager:
         self.client = client
         self._voice_states_data = {}
         self.voice_update_callback_handle = client.event_voice_state_update.callback_add(self._on_voice_state_update)
-        self.event_voice_channel_joined: EventDispenser[Tuple[str, str, str]] = EventDispenser(client.event_loop)
+        self.event_voice_channel_changed: EventDispenser[Tuple[str, str, str]] = EventDispenser(client.event_loop)
         
     def _on_voice_state_update(self, event_data: dict):
         user_id = event_data['user_id']
@@ -20,6 +20,10 @@ class VoiceStateManager:
         try:
             new_channel_id =  event_data['channel_id']
         except KeyError:
-            pass
+            new_channel_id = None
+
+        # TODO: mute and etc.
 
         self._voice_states_data[user_id] = event_data
+
+        self.event_voice_channel_changed.put((user_id, old_channel_id, new_channel_id))
